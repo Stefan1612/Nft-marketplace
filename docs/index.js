3a2c5a2482b0e2d2b7f9fd3d277b0f7f3ddaa487
Stefan Lehmann <85541624+Stefan1612@users.noreply.github.com>
2022-01-18 00:55:49 +0100

    Update index.js

--- docs/index.js
+++ docs/index.js
@@ -1,1 +1,1 @@
-<meta http-equiv="refresh" content="0; url=https://repo.github.io/folder/index.html">
+<meta http-equiv="refresh" content="0; url=https://Nft-marketplace.github.io/..public/index.html">
